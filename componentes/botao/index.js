97ceaa08f0972d9cd4d8635b56fc9811cb7d1d93
Otavio Dias Marques <otavio-79@hotmail.com>
2022-04-06 23:17:27 -0300

    botao e avatar

---
+++ componentes/botao/index.js
@@ -0,0 +1,17 @@
+export default function Botao({
+    type = 'button',
+    texto,
+    cor = 'primaria',
+    desabilitado = false,
+    manipularClique 
+}) {    return (
+        <button
+           type={type}
+           className={`btn ${cor} `}
+           disabled={desabilitado}
+           onClick={manipularClique}
+        >
+            {texto}
+        </button>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Botao from '../componentes/botao'
+import Avatar from '../componentes/avatar'
 
 export default function Home() {
   return (
-    <h1>Olá mundo</h1>    
+    <>
+      <h1>Olá mundo</h1>
+      <div style={{width: 200}}>
+       <Avatar />
+       <Botao texto={'Login'} cor='primaria' manipularClique={() => console.log('botao clicado')} />
+       </div>  
+    </>
   )
 }
